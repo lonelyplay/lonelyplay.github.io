cf354d513049b281591e55ff45b61592bacbd014
lonelyplay <952223695@qq.com>
2026-04-21 15:39:17 +0800

    Site updated: 2026-04-21 15:39:13

---
+++ js/RightMenu.js
@@ -0,0 +1,279 @@
+// RightMenu.js
+var right_click_num = 0;
+var rect = document.getElementById("rightmenu-content").getBoundingClientRect();
+window.oncontextmenu = function(e){
+	// 检查是否按下了Ctrl键
+	if (e.ctrlKey) {
+		return true;
+	}
+
+	e.preventDefault(); //阻止浏览器自带的右键菜单显示
+	var menu = document.getElementById("rightmenu-wrapper");
+	menu.style.display = "block"; //将自定义的“右键菜单”显示出来
+    if (e.clientX + rect.width > window.innerWidth) { // 如果超出屏幕右边
+        if (e.clientX - rect.width < 0) {
+            menu.style.left = "0px"; // 如果超出屏幕左边，则调整位置到左侧贴边
+        } else {
+            menu.style.left = e.clientX - rect.width + "px"; // 否则调整到鼠标位置的左侧
+        }
+    } else {
+        menu.style.left = e.clientX + "px"; // 设置位置，跟随鼠标
+    }
+    if (e.clientY + rect.height > window.innerHeight) { // 如果超出屏幕底部
+        if (e.clientY - rect.height < 0) {
+            menu.style.top = "0px"; // 如果超出屏幕顶部，则调整位置到顶部
+        } else {
+            menu.style.top = e.clientY - rect.height + "px"; // 否则调整到鼠标位置的上方
+        }
+    } else {
+        menu.style.top = e.clientY + "px"; // 设置位置，跟随鼠标
+    }
+
+	right_click_num = right_click_num + 1; //右键点击次数加1
+
+	if(right_click_num == 1){
+        showTooltip({
+            type: 'info',
+            message: '如需原始右键菜单请按下 <strong>Ctrl+右键</strong>'
+        });
+	}
+}
+
+window.onclick = function(e){ //点击窗口，右键菜单隐藏
+	var menu = document.getElementById("rightmenu-wrapper");
+	menu.style.display = "none";
+}
+
+// 监听鼠标右键按下事件
+document.addEventListener('contextmenu', function(event) {    
+    // 检查是否有选中的文本
+    const copySelectedTextItem = document.getElementById('copy-selected-text');
+    const searchSelectedTextItem = document.getElementById('search-selected-text-BING');
+    const selectedText = window.getSelection().toString().trim();
+    if (selectedText) {
+        copySelectedTextItem.hidden = false;
+        searchSelectedTextItem.hidden = false;
+    } else {
+        copySelectedTextItem.hidden = true;
+        searchSelectedTextItem.hidden = true;
+    }
+
+    // 检查是否有链接被点击
+    const goToLinkItem = document.getElementById('go-to-link');
+    const link = event.target.closest('a[href]');
+    if (link) {
+        goToLinkItem.hidden = false;
+        goToLinkItem.querySelector('a').setAttribute('href', link.href);
+    } else {
+        goToLinkItem.hidden = true;
+    }
+
+    //检查是否有图片被点击
+    const downloadImageItem = document.getElementById('download-image');
+    const copyImageItem = document.getElementById('copy-image-link');
+    const img = event.target.closest('img') || event.target.closest('svg');
+    if (img) {
+        downloadImageItem.hidden = false;
+        if (img.tagName.toLowerCase() === 'img') {
+        copyImageItem.hidden = false;
+        downloadImageItem.querySelector('a').setAttribute('onclick', `downloadImage('${img.src}')`);
+        copyImageItem.querySelector('a').setAttribute('onclick', `copyImageLink('${img.src}')`);
+        } else if (img.tagName.toLowerCase() === 'svg' && img.classList.contains('custom-gallery-svg')) {
+            const backgroundImage = img.style.backgroundImage;
+            const urlMatch = backgroundImage.match(/url\(["']?(.*?)["']?\)/);
+            if (urlMatch && urlMatch[1]) {
+            const imageUrl = urlMatch[1];
+            downloadImageItem.querySelector('a').setAttribute('onclick', `downloadImage('${imageUrl}')`);
+            copyImageItem.querySelector('a').setAttribute('onclick', `copyImageLink('${imageUrl}')`);
+            }
+        }
+    } else {
+        downloadImageItem.hidden = true;
+        copyImageItem.hidden = true;
+    }
+
+    // 根据前三者判断第一栏是否有元素，需要分割线
+    const topLineItem = document.getElementById('top-line');
+    if (copySelectedTextItem.hidden && !link && !img) {
+        topLineItem.hidden = true;
+    } else {
+        topLineItem.hidden = false;
+    }
+    
+    // 更新尺寸相关参数
+    rect = document.getElementById("rightmenu-content").getBoundingClientRect();
+});
+
+// 平滑滚动到顶部
+function scrollToTopSmooth() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// 平滑滚动到底部
+function scrollToBottomSmooth() {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+}
+
+// 复制选中-复制选中文本功能
+function copySelectedText() {
+    const selectedText = window.getSelection().toString();
+    if (selectedText) {
+        navigator.clipboard.writeText(selectedText);
+        showTooltip({
+            type: 'success',
+            message: '选中文本已复制到剪贴板'
+        });
+    }
+}
+
+// 必应搜索-在Bing上搜索选中文本功能
+function searchSelectedTextBing() {
+    const selectedText = window.getSelection().toString().trim();
+    if (selectedText) {
+        const bingSearchUrl = `https://www.bing.com/search?q=${encodeURIComponent(selectedText)}`;
+        window.open(bingSearchUrl, '_blank');
+    }
+}
+
+// 下载图片-下载选中图片功能
+function downloadImage(imgsrc) {
+    if (!imgsrc) {
+        console.error('Image source is required');
+        return;
+    }
+
+    const name = imgsrc.split('/').pop(); // Extract the image name from the path
+    fetch(imgsrc)
+        .then(response => response.blob())
+        .then(blob => {
+            const a = document.createElement('a');
+            const url = URL.createObjectURL(blob);
+            a.href = url;
+            a.download = name;
+            document.body.appendChild(a);
+            a.click();
+            URL.revokeObjectURL(url);
+            document.body.removeChild(a);
+            showTooltip({
+                type: 'success',
+                message: '图片已下载'
+            });
+        })
+        .catch(error => console.error('Error downloading image:', error));
+}
+
+// 复制图片链接-复制选中图片链接功能
+function copyImageLink(imgsrc) {
+    if (imgsrc) {
+        navigator.clipboard.writeText(imgsrc);
+        showTooltip({
+            type: 'success',
+            message: '链接已复制到剪贴板'
+        });
+    }
+}
+
+// 随便看看-随机跳转到文章
+function RandomGo() {
+    var posts = JSON.parse(sessionStorage.getItem('posts')) || [];
+    if (posts.length === 0) {
+        fetch('/sitemap.xml')
+        .then(response => response.text())
+        .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+        .then(data => {
+            const entries = data.querySelectorAll('url > loc');
+            posts = Array.from(entries)
+                .map(entry => entry.textContent)
+                .filter(link => link.includes('/_posts/'))
+                .map(link => link.substring(link.indexOf('/_posts/')));
+            sessionStorage.setItem('posts', JSON.stringify(posts)); // 保存到 sessionStorage
+            console.log('Posts updated:', posts); // 调试输出更新后的链接列表
+            const randomIndex = Math.floor(Math.random() * posts.length);
+            const randomLink = posts[randomIndex];
+            window.location.href = randomLink;       
+        })
+        .catch(error => console.error('Error fetching sitemap:', error));
+    } else {
+        const randomIndex = Math.floor(Math.random() * posts.length);
+        const randomLink = posts[randomIndex];
+        window.location.href = randomLink;
+    }
+}
+
+// 复制链接-复制当前地址功能
+function copyLink() {
+    const link = window.location.href;
+    navigator.clipboard.writeText(link);
+    showTooltip({
+        type: 'success',
+        message: '链接已复制到剪贴板'
+    });
+}
+
+// 切换昼夜模式功能
+function toggleColorMode() {
+    document.getElementById('color-toggle-btn').click();
+    if (document.documentElement.getAttribute('data-user-color-scheme') === 'dark' ||
+        (document.documentElement.getAttribute('data-user-color-scheme') === 'auto' &&
+         window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.getElementById('toggle-color-mode-icon').className = "iconfont icon-dark";
+    }
+    else {
+        document.getElementById('toggle-color-mode-icon').className = "iconfont icon-light";
+    }
+}
+
+// 切换全屏背景功能
+function toggleBackgroundMode() {
+    const BackgroundMode = localStorage.getItem('BackgroundMode');
+    if (BackgroundMode === 'false' || !BackgroundMode) {
+        document.querySelector('#web_bg').style.backgroundImage = `${document.querySelector('.banner').style.background.split(' ')[0]}`;
+        document.querySelector("#banner").style.background = 'url()';
+        document.querySelector("#banner .mask").style.backgroundColor = 'rgba(0,0,0,0)';
+        document.getElementById('toggle-background-mode-icon').className = "fa-solid fa-toggle-on";
+        ['#toc', '.category-list'].forEach(selector => {
+            if (document.querySelector(selector)) {
+                document.querySelector(selector).style.backgroundColor = "var(--board-bg-color)";
+            }
+        });
+        localStorage.setItem('BackgroundMode', 'true');
+    }
+    else {
+        document.querySelector("#banner").style.background = document.querySelector('#web_bg').style.backgroundImage + " center center / cover no-repeat";
+        document.querySelector('#web_bg').style.backgroundImage = 'url()';
+        document.querySelector("#banner .mask").style.backgroundColor = 'rgba(0,0,0,0.3)';
+        document.getElementById('toggle-background-mode-icon').className = "fa-solid fa-toggle-off";
+        ['#toc', '.category-list'].forEach(selector => {
+            if (document.querySelector(selector)) {
+                document.querySelector(selector).style.removeProperty('background-color');
+            }
+        });
+        localStorage.setItem('BackgroundMode', 'false');
+    }
+}
+
+// 页面加载完毕后初始化图标状态和提示
+window.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem('DarkNightMode') === 'true') {
+        document.getElementById("toggle-color-mode-icon").className = "iconfont icon-light";
+    } else {
+        document.getElementById("toggle-color-mode-icon").className = "iconfont icon-dark";
+    }
+    if (localStorage.getItem('BackgroundMode') === 'true') {
+        document.getElementById('toggle-background-mode-icon').className = "fa-solid fa-toggle-on";
+    } else {
+        document.getElementById('toggle-background-mode-icon').className = "fa-solid fa-toggle-off";
+    }
+
+    // 显示提示气泡
+    showTooltip({
+        type: 'info',
+        message: '如需原始右键菜单请按下 <strong>Ctrl+右键</strong>'
+    });
+});
